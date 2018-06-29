75b80e9ad056b62972b55eeb61d6e39a9af8c252
Roman Chuprikov <paradise.develop@gmail.com>
2018-06-29 13:36:05 +0500

    LWDEV-8153: prevent memory leak

---
+++ client/Lykke.Service.TradesAdapter.Client/TradesAdapterAPI.Extend.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+// ReSharper disable once CheckNamespace
+namespace Lykke.Service.TradesAdapter.AutorestClient
+{
+    public partial class TradesAdapterAPI
+    {
+        /// <inheritdoc />
+        /// <summary>
+        /// Should be used to prevent memory leak in RetryPolicy
+        /// </summary>
+        public TradesAdapterAPI(Uri baseUri, HttpClient client) : base(client)
+        {
+            Initialize();
+
+            BaseUri = baseUri ?? throw new ArgumentNullException("baseUri");
+        }
+    }
+}
